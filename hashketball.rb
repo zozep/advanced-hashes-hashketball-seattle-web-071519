1b28a9bead26ae4a26ff12b7228f5cdff6dd8da9
Joseph P <jpark041@gmail.com>
2019-07-15 07:05:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,13 +155,12 @@ end
 
 def player_numbers(team_name)
   nums = []
-  
   game_hash.each do |_place, team|
     next unless team[:team_name] == team_name
-    
+
     team.each do |attribute, data|
       next unless attribute == :players
-      
+
       data.each do |data|
         nums << data[:number]
       end
